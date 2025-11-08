b14fee053dc83f1da4d8b16f64f1561d090fec1a
Andrew Kondratev <andruhon@gmail.com>
2025-11-09 01:36:56 +1300

    don't braker 1.0 docs

--- update-docs.sh
+++ update-docs.sh
@@ -1,3 +1,3 @@
 npm run typedoc
-rm -r ../gaunt-sloth-assistant.github.io/docs
-cp -r docs-generated ../gaunt-sloth-assistant.github.io/docs
+rm -r ../gaunt-sloth-assistant.github.io/docs-0.9
+cp -r docs-generated ../gaunt-sloth-assistant.github.io/docs-0.9
